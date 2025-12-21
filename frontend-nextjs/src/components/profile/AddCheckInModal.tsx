742f51c9a863867a71ff9230cd63cef5331a27e5
Prohmpiriya Phonunumnuaisuk <nut.prohmpiriya@gmail.com>
2025-12-21 17:35:38 +0700

    fix: update tile layer URL and attribution to use CARTO in map components

--- frontend-nextjs/src/components/profile/AddCheckInModal.tsx
+++ frontend-nextjs/src/components/profile/AddCheckInModal.tsx
@@ -92,8 +92,8 @@ const FullscreenMap = dynamic(() => import('react-leaflet').then(async (mod) => 
                 worldCopyJump={true}
             >
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
                 <ZoomControl position="bottomright" />
                 <MapController
--- frontend-nextjs/src/components/profile/CheckInSection.tsx
+++ frontend-nextjs/src/components/profile/CheckInSection.tsx
@@ -52,8 +52,8 @@ const MapWithMarkers = dynamic(() => import('react-leaflet').then(async (mod) =>
                 attributionControl={true}
             >
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
                 {checkins.map((checkin, index) => (
                     <Marker
